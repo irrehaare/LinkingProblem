c251a56dd9030b2dcbf7b405d8fbfa11771b68ca
irrehaare <wjtkwilk@gmail.com>
2025-02-18 22:42:04 +0100

    Add comment

--- LinkingProblem/src/common/GameState.hpp
+++ LinkingProblem/src/common/GameState.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <vector>
-#include <Entity.hpp>
+#include <Entity.hpp> // Why does it work?! 
+// Where is "cannot open source file "Entity.hpp"?
 
 struct GameState {
     explicit GameState() = default;
